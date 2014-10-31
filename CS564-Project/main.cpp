a2b61e49586ca61c6c304429a6523828a6760836
PatrickWu137 <wuwentao137@gmail.com>
2014-10-30 19:48:56 -0500

    Initial Commit

---
+++ CS564-Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS564-Project
+//
+//  Created by Wentao Wu on 10/30/14.
+//  Copyright (c) 2014 Wentao Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
